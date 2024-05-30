652d149497faa7f7e45d9aa7d2ef5b41a9398e89
mksmmirhs <meraj.exe@gmail.com>
2024-05-30 07:52:16 +0600

    jwt done

--- SimpleServer/index.js
+++ SimpleServer/index.js
@@ -4,31 +4,38 @@ require("dotenv").config();
 const fs = require("fs");
 const path = require("path");
 const app = express();
-
+const jwt = require("jsonwebtoken");
 const port = process.env.PORT || 5000;
 
 //parsers
 app.use(express.json());
 app.use(cors());
 
-// read file and return json
-
-// const readLocalFile = () => {
-//   const filePath = path.join(__dirname, "data.json");
-//   fs.readFile(filePath, "utf8", (err, data) => {
-//     if (err) {
-//       return res.status(500).json({ error: "Unable to read file" });
-//     }
-
-//     return JSON.parse(data);
-//   });
-// };
-
 // main function
 async function run() {
   try {
-    app.get("/data", (req, res) => {
-      const data = require("./data.json");
+    // user login route
+    app.post("/login", (req, res) => {
+      const { users } = require("./users.json");
+      let loggedInUser = null;
+      // check if the logged in user on jason file
+      users.forEach((user) => {
+        if (
+          user.username === req.body.username &&
+          user.password === req.body.password
+        ) {
+          loggedInUser = user;
+          // create jwt token
+          const jwtToken = jwt.sign(loggedInUser, process.env.JWT_SECRET, {
+            expiresIn: "3h",
+          });
+          res.status(200).send(jwtToken);
+        }
+      });
+      //  send status for unauthorized access
+      if (!loggedInUser) {
+        res.status(401).send("Unauthorized");
+      }
     });
   } finally {
   }
